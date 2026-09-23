b34db62cf78f3d5bdf987231ecf04d2ac7684812
Olga <OlgaTimofeeva8909@yandex.ru>
2026-09-23 13:35:30 +0300

    Исправила  деплой и настройки

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,18 +1,17 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'kittygram2026.ddnsfree.com',
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv(
+    'ALLOWED_HOSTS'
+) else []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
